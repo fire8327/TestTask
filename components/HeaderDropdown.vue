8a45759fd2e312a1a1413f124fe99761af8211c0
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-10-21 23:10:40 +0300

    upd

--- components/HeaderDropdown.vue
+++ components/HeaderDropdown.vue
@@ -1,17 +1,21 @@
 <template>
-    <div class="relative group">
-        <button @click="isDown = !isDown" class="flex items-center gap-4">
+    <div class="relative group">    
+        <button @click="isDown = !isDown" v-on-click-outside="closeDropdown" class="flex items-center gap-4">
             <p>{{ title }}</p>
             <Icon name="ic:outline-keyboard-arrow-down" class="text-xl xl:opacity-0 transition-all duration-500" :class="isDown ? 'xl:opacity-100' : 'xl:group-hover:opacity-100'"/>
         </button>
         <div class="absolute z-50 top-[calc(100%+10px)] shadow-[3px_2px_16px_0px_rgba(0,0,0,0.10)] w-fit flex flex-col rounded-lg transition-all duration-500 bg-white -translate-x-1/2 left-1/2" :class="{'hidden' : !isDown}">
-            <NuxtLink @click="isDown = false" v-for="content in contents" :to="content.link" class="transition-all duration-500 hover:bg-gray-200 px-4 py-1.5 first:rounded-t-lg last:rounded-b-lg">{{ content.title }}</NuxtLink>
+            <NuxtLink @click="closeDropdown = false" v-for="content in contents" :to="content.link" class="transition-all duration-500 hover:bg-gray-200 px-4 py-1.5 first:rounded-t-lg last:rounded-b-lg">{{ content.title }}</NuxtLink>
         </div>
     </div>
 </template>
 
 <script setup>
+    import { vOnClickOutside } from '@vueuse/components'
     const isDown = ref(false)
+    function closeDropdown() {
+        isDown.value = false
+    }
     defineProps({
         title: String,
         contents: Array
