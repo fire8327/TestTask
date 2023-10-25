e36f9bf5920eba017d1960396d2eb4af8f340abc
fire8327 <fire83274@gmail.com>
2023-10-25 15:36:35 +0300

    upd

--- components/Header.vue
+++ components/Header.vue
@@ -37,7 +37,7 @@
                                 <Icon name="ph:gift" class="text-white text-sm"/>
                             </span>
                         </button>
-                        <div class="absolute z-50 top-[calc(100%+10px)] shadow-[3px_2px_16px_0px_rgba(0,0,0,0.10)] w-fit flex flex-col rounded-lg transition-all duration-500 bg-white -translate-x-1/2 left-1/2" :class="{'hidden' : !isProfileDown}">
+                        <div class="absolute z-50 top-[calc(100%+10px)] shadow-[3px_2px_16px_0px_rgba(0,0,0,0.10)] w-fit flex flex-col rounded-lg transition-all duration-500 bg-white max-xl:left-1/2 max-xl:-translate-x-1/2 right-0" :class="{'hidden' : !isProfileDown}">
                             <NuxtLink @click="closeProfile" class="transition-all duration-500 hover:bg-gray-200 px-4 py-1.5 first:rounded-t-lg last:rounded-b-lg">Профиль</NuxtLink>
                             <NuxtLink @click="closeProfile" class="transition-all duration-500 hover:bg-gray-200 px-4 py-1.5 first:rounded-t-lg last:rounded-b-lg">Зар.плата</NuxtLink>
                             <NuxtLink to="/login" @click="closeProfile" class="transition-all duration-500 hover:bg-gray-200 px-4 py-1.5 first:rounded-t-lg last:rounded-b-lg border-t border-[#D1CFD7] text-[#34B846]">Войти</NuxtLink>
