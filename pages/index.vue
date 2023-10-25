9265c1b7337644063b6c22e955137ddb498e98f2
fire8327 <fire83274@gmail.com>
2023-10-25 16:30:20 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,13 @@
 <template>
     <div class="absolute w-full h-full z-50 inset-0 flex items-center justify-center text-[#1D1D1D] bg-white">
-        <div class="flex flex-col items-center gap-12 w-full md:w-1/2 xl:w-1/3 px-[15px] sm:px-5">
+        <FormKit type="form" form-class="flex flex-col items-center gap-12 w-full md:w-1/2 xl:w-1/3 px-[15px] sm:px-5" :actions="false">
             <img src="/images/header/logo.png" alt="" class="w-fit">
             <div class="flex flex-col gap-6 w-full">
-                <div class="flex flex-col gap-3">
-                    <p class="font-medium">Логин</p>
-                    <input type="text" class="w-full rounded-lg border-[0.6px] border-[#C3C3C3] py-4 px-7" placeholder="Введите ваш логин">
-                </div>
-                <div class="flex flex-col gap-3">
-                    <p class="font-medium">Пароль</p>
-                    <input type="text" class="w-full rounded-lg border-[0.6px] border-[#C3C3C3] py-4 px-7" placeholder="Введите ваш пароль">
-                </div>
+                <FormKit type="text" validation="required|length:5" inner-class="w-full $remove:max-w-md" input-class="w-full rounded-lg border-[0.6px] border-[#C3C3C3] py-4 px-7" message-class="text-[#E71616]" wrapper-class="flex flex-col gap-3" label="Логин" placeholder="Введите ваш логин"/>
+                <FormKit type="text" validation="required|length:5" inner-class="w-full $remove:max-w-md" input-class="w-full rounded-lg border-[0.6px] border-[#C3C3C3] py-4 px-7" message-class="text-[#E71616]" wrapper-class="flex flex-col gap-3" label="Пароль" placeholder="Введите ваш пароль"/>
             </div>
             <NuxtLink to="/workers" class="w-full text-lg text-white rounded-lg bg-[#1F75A8] font-medium py-3.5 text-center">Войти</NuxtLink>
-        </div>   
+        </FormKit>   
     </div>
 </template>
 
