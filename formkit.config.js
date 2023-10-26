552dc55e8899930fddcce69066ffaccaf5f1ea27
fire8327 <fire83274@gmail.com>
2023-10-26 11:51:41 +0300

    upd form

--- formkit.config.js
+++ formkit.config.js
@@ -1,11 +1,6 @@
-import { generateClasses } from '@formkit/themes'
 import { ru } from '@formkit/i18n'
-import genesis from '@formkit/themes/tailwindcss/genesis'
 
 export default {
     locales: { ru },
-    locale: 'ru',
-    config: {
-        classes: generateClasses(genesis),
-    }
+    locale: 'ru'
 }
